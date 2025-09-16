0e2b87862d2f89b7eec2ecd0934112478e8e1b62
Aghnaya <nay.kenara@gmail.com>
2025-09-17 00:56:12 +0700

    Tugas 3

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "aghnaya-kenarantanov-footballshop.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://aghnaya-kenarantanov-footballshop.pbp.cs.ui.ac.id",
+]
 
 # Application definition
 
